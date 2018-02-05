487fa0630aef62b5999aa9b528afc34d5fb7e32c
BT_1N3_12 <BT_1N3_12@BT_1N3_12.bktech.local>
2018-02-05 09:29:59 -0500

    HArdcoding?

--- Lab 3.2/src/ArrayMethods2.java
+++ Lab 3.2/src/ArrayMethods2.java
@@ -1,5 +1,24 @@
 
 public class ArrayMethods2 
 {
-	
+	public static int[] merge(int[] arr1, int[] arr2)
+	{
+		int[] result = new int[arr1.length + arr2.length];
+		int index1 = 0;
+		int index2 = 0;
+		int indexresult = 0;
+		while(result[arr1.length + arr2.length - 1] == 0)
+		{
+			if (arr1[index1] <= arr2[index2])
+			{
+				result[indexresult] = arr1[index1];
+				index1++;
+				indexresult++;
+			}
+			
+			
+		}
+		return result;
+		
+	}
 }
