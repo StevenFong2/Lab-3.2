2e5d6542516eafd665ceb804b2476b5f5dcb72ca
BT_1N3_14 <BT_1N3_14@BT_1N3_14.bktech.local>
2018-02-06 09:27:28 -0500

    wow

--- Lab 3.2/src/ArrayMethods2.java
+++ Lab 3.2/src/ArrayMethods2.java
@@ -1,24 +1,51 @@
 
 public class ArrayMethods2 
 {
-	public static int[] merge(int[] arr1, int[] arr2)
+	public static String[] merge(String [] arr1, String[] arr2)
 	{
-		int[] result = new int[arr1.length + arr2.length];
+		String[] result = new String[arr1.length + arr2.length];
 		int index1 = 0;
 		int index2 = 0;
 		int indexresult = 0;
-		while(result[arr1.length + arr2.length - 1] == 0)
+		while(result[arr1.length + arr2.length - 1] == null)
 		{
-			if (arr1[index1] <= arr2[index2])
+			if (arr1[index1].compareTo(arr2[index2]) <= 0)
 			{
 				result[indexresult] = arr1[index1];
 				index1++;
 				indexresult++;
 			}
 			
+			if (index1 == arr1.length)
+			{
+				for (int i = index2; i < arr2.length; i++)
+				{
+					result[indexresult] = arr2[i];
+					indexresult++;
+				}
+			}
+			
+			if (index2 == arr2.length)
+			{
+				for (int i = index1; i < arr1.length; i++)
+				{
+					result[indexresult] = arr2[i];
+					indexresult++;
+				}
+			}
+			
+			else
+			{
+				result[indexresult] = arr2[index2];
+				index2++;
+				indexresult++;
+			}
 			
 		}
+		
 		return result;
 		
 	}
+	
+	public static String[] partition()
 }
