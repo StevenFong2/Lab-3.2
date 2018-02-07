5c30735e7bdbaf4928d66f6a9e21e9c87c990253
Steven <Steven@Steven>
2018-02-06 20:04:01 -0500

    hmmm...

--- Lab 3.2/src/ArrayMethods2.java
+++ Lab 3.2/src/ArrayMethods2.java
@@ -7,45 +7,100 @@ public class ArrayMethods2
 		int index1 = 0;
 		int index2 = 0;
 		int indexresult = 0;
-		while(result[arr1.length + arr2.length - 1] == null)
+		/*while(indexresult < result.length)
 		{
-			if (arr1[index1].compareTo(arr2[index2]) <= 0)
+			if (index1 == arr1.length)
+			{
+				result[indexresult] = arr2[index2];
+				index2++;
+			}
+			
+			else if (index2 == arr2.length)
+			{
+				result[indexresult] = arr2[index1];
+				index1++;
+			}
+			
+			else if (arr1[index1].compareTo(arr2[index2]) <= 0)
 			{
 				result[indexresult] = arr1[index1];
 				index1++;
+			}
+			
+			else if (arr1[index1].compareTo(arr2[index2]) > 0)
+			{
+				result[indexresult] = arr2[index2];
+				index2++;
+			}
+			indexresult++;
+			
+		}*/
+		
+		while (indexresult < result.length)
+		{
+			while(index2 < arr2.length && index1 < arr1.length)
+			{
+				if (arr1[index1].compareTo(arr2[index2]) <= 0)
+				{
+					result[indexresult] = arr1[index1];
+					index1++;
+				}
+				else if (arr1[index1].compareTo(arr2[index2]) > 0)
+				{
+					result[indexresult] = arr2[index2];
+					index2++;
+				}
 				indexresult++;
 			}
 			
 			if (index1 == arr1.length)
-			{
-				for (int i = index2; i < arr2.length; i++)
-				{
-					result[indexresult] = arr2[i];
-					indexresult++;
-				}
-			}
-			
-			if (index2 == arr2.length)
-			{
-				for (int i = index1; i < arr1.length; i++)
-				{
-					result[indexresult] = arr2[i];
-					indexresult++;
-				}
-			}
-			
-			else
 			{
 				result[indexresult] = arr2[index2];
 				index2++;
 				indexresult++;
 			}
 			
+			else if (index2 == arr2.length)
+			{
+				result[indexresult] = arr1[index1];
+				index1++;
+				indexresult++;
+			}
 		}
-		
 		return result;
-		
 	}
 	
-	public static String[] partition()
+	public static int partition(int[] list)
+	{
+		int pivot = list[0];
+		//int pivotpos = 0;
+		//int rightpos = list.length - 1;
+		/*for (int i = 1; i < list.length; i++)
+		{
+			if (list[i] < pivot)
+			{
+				int sub = list[pivotpos];
+				list[i] = list[pivotpos];
+				list[pivotpos] = sub;
+				pivotpos = i;
+				rightpos++;
+			}
+		}
+		int sub = list[pivotpos];
+		list[pivotpos] = list[rightpos];
+		list[rightpos] = sub;*/
+		
+		int pivotpos = list.length - 1;
+		for (int i = list.length - 1; i >= 0; i--)
+		{
+			if (list[i] > pivot)
+			{
+				int sub = list[pivotpos];
+				list[pivotpos] = list[i];
+				list[i] = sub;
+				pivotpos++;
+			}
+		}
+		return pivotpos;
+	}
 }
