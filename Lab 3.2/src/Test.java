c65a5c6cb13402199e6450943a24eff7884ceac5
BT_1N3_16 <BT_1N3_16@BT_1N3_16.bktech.local>
2018-02-09 09:06:49 -0500

    When Am I Never Done

--- Lab 3.2/src/Test.java
+++ Lab 3.2/src/Test.java
@@ -4,7 +4,7 @@ public class Test
 {
 	public static void main(String[] args)
 	{
-		 String[] one = {"abba", "book", "cat", "dog","wow", "zoo", "zzzz"};
+		 /*String[] one = {"abba", "book", "cat", "dog","wow", "zoo", "zzzz"};
 		 String[] two = {"bee", "die", "fall", "good", "mummy"};
 		 print(ArrayMethods2.merge(one, two));
 		 
@@ -12,7 +12,27 @@ public class Test
 		 int[] b = {10,8,9,35,26,48,100,104,29,5,8,3,87};
 		 //System.out.println(ArrayMethods2.partition(a));
 		 ArrayMethods2.partition(b);
-		 print(b);
+		 print(b);*/
+		
+		String [] test1 = {"apple", "cucumber", "microsoft", "zorro"};
+		String [] test2 = {"banana", "cherry", "mahogany", "oreos", "pinata"};
+		int [] test3 = {3,4,2,7,12,22,0,1};
+		
+		long start = System.nanoTime();
+		String [] mergeResult = ArrayMethods2.merge(test1, test2);
+		long end = System.nanoTime();
+		long time = end - start;
+		System.out.println("Merge test took: " + time + " nanoseconds");
+		print(mergeResult);
+		
+		start = System.nanoTime();
+		int pivotFinalPos = ArrayMethods2.partition(test3);
+		end = System.nanoTime();
+		time = end - start;
+		System.out.println("Partition test took: " + time + " nanoseconds");
+		System.out.println("Final Pivot Position: " + pivotFinalPos);
+		print(test3);
+		
 	}
 	
 	public static void print(String[] wow)
